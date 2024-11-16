b6e5c17de461a6bfc0eaa7dac47bfe9538bb3801
samuel.mugisha <samuel.mugisha@codeland.it>
2024-11-16 12:42:41 +0200

    initialize project

---
+++ src/main/java/com/samuel/email_signature_generator/EmailSignatureGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.samuel.email_signature_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailSignatureGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailSignatureGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samuel/email_signature_generator/EmailSignatureGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samuel.email_signature_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailSignatureGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
